9fc03896278f255496750febc88e325d923ee2d7
Rene Hexel <r.hexel@griffith.edu.au>
2016-05-29 15:16:45 +1000

    Add dependencies

--- Package.swift
+++ Package.swift
@@ -1,5 +1,9 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftGdkPixbuf"
+    name: "SwiftGdkPixbuf",
+    dependencies: [
+        .Package(url: "https://github.com/rhx/SwiftGIO.git", majorVersion: 2),
+        .Package(url: "https://github.com/rhx/SwiftGModule.git", majorVersion: 2)
+    ]
 )
