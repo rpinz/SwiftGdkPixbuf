4919e95a83639f130cbc809cece662ab8a7d49e7
Rene Hexel <r.hexel@griffith.edu.au>
2016-07-31 14:31:49 +1000

    Compile with Swift 3.0 preview 3

--- config.sh
+++ config.sh
@@ -6,6 +6,6 @@
 Mod=GdkPixbuf
 Module=${Mod}-2.0
 module=gdk-pixbuf-2.0
-export PATH=/Library/Developer/Toolchains/swift-latest.xctoolchain/usr/bin:"${PATH}"
+export PATH=`pwd`/.build/gir2swift/.build/debug:"${PATH}"
 LINKFLAGS=`pkg-config --libs $module gio-unix-2.0 glib-2.0 | tr ' ' '\n' | sed 's/^/-Xlinker /' | tr '\n' ' '`
 CCFLAGS=`pkg-config --cflags $module  gio-unix-2.0 glib-2.0 | tr ' ' '\n' | sed 's/^/-Xcc /' | tr '\n' ' ' `
