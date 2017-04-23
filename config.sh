17fc7287b048a0d6ceb4a652545a92fe39a56cd0
Rene Hexel <r.hexel@griffith.edu.au>
2017-04-24 07:22:08 +1000

    Add path to release mode gir2swift

--- config.sh
+++ config.sh
@@ -6,7 +6,8 @@
 Mod=GdkPixbuf
 Module=${Mod}-2.0
 module=gdk-pixbuf-2.0
-export PATH=`pwd`/.build/gir2swift/.build/debug:"${PATH}"
+BUILD_DIR=`pwd`/.build
+export PATH="${BUILD_DIR}/gir2swift/.build/release:${BUILD_DIR}/gir2swift/.build/debug:${PATH}"
 LINKFLAGS=`pkg-config --libs $module gio-unix-2.0 glib-2.0 | tr ' ' '\n' | sed 's/^/-Xlinker /' | tr '\n' ' '`
 CCFLAGS=`pkg-config --cflags $module  gio-unix-2.0 glib-2.0 | tr ' ' '\n' | sed 's/^/-Xcc /' | tr '\n' ' ' `
 TAC="tail -r"
