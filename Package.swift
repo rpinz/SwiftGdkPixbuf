29f0be1e3255150ac21e52279e7ec9791f3432dc
Rene Hexel <r.hexel@griffith.edu.au>
2018-04-22 06:30:38 +1000

    Make this compile with Swift 4

--- Package.swift
+++ Package.swift
@@ -7,5 +7,5 @@ let package = Package(
         .Package(url: "https://github.com/rhx/SwiftGIO.git", majorVersion: 2),
         .Package(url: "https://github.com/rhx/SwiftGModule.git", majorVersion: 2)
     ],
-    swiftLanguageVersions: [3]
+    swiftLanguageVersions: [3, 4]
 )
